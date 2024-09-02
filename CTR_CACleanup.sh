4d38f7275b73cf89ece160d79ac84b1c242b7f8d
igmuhlmann <75082429+igmuhlmann@users.noreply.github.com>
2024-09-02 13:54:13 +0200

    Update CTR_CACleanup.sh

--- CTR_CACleanup.sh
+++ CTR_CACleanup.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Script to continously clear up continous data archive
@@ -6,18 +7,22 @@
 # Should be amended to the Continous Archive String in the long run
 # Script needs to be run as cronjob and will log to /var/log/syslog
 
+echo "Archive Cleanup Started"
 
 # This is the treshold, given as input argument
 treshold=$1
 
 # Check the disk usage of /media/removable0
 usage=$(df /media/removable0 | awk 'NR==2 {print $5}' | sed 's/%//')
-# Check if usage is greater than 80%
+
+logger "Continous Archive Cleanup initiated (Usage $usage Treshold $treshold)"
+
+# Check if usage is greater than treshold
 while [ "$usage" -gt "$treshold" ]
 do
   # Continously update usage
   usage=$(df /media/removable0 | awk 'NR==2 {print $5}' | sed 's/%//')
-  echo "Continous Archive $usage% exceeds limit of $treshold%. Deleting oldest file."
+  # echo "Continous Archive $usage% exceeds limit of $treshold%. Deleting oldest file."
   # Navigate to the base directory
   cd /media/removable0/
   # Find the folder with the lowest year number
@@ -30,12 +35,14 @@ do
   # Find the oldest .miniseed file within the target folder
   filename=$(find . -type f -name "*.miniseed" -printf '%p\n' | sort | head -n 1)
   fullname=$(echo "$(pwd)/$filename")
-
-  rm -rf `echo $fullname`
-  echo $fullname file deleted.
-  logger "Continous Archive usage was exeeding treshold ($usage%>$treshold%). File $fullname deleted."
+  if [ $fullname = "//" ]
+  then
+        echo "Error. No File present."
+        exit
+  else
+        rm -rf `echo $fullname`
+        echo $fullname file deleted.
+        logger "Continous Archive treshold ($usage%>$treshold%): $fullname deleted."
+  fi
 done
-
-
-
 
